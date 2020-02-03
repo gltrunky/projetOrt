082b5cb3b8533cb235f66a81b59b17578071e881
trunky <almartin86@yahoo.fr>
2020-02-03 11:51:57 +0100

    New Project Feu Triclore

---
+++ feuTricolor/js/app.js
@@ -0,0 +1,80 @@
+//DECLARATION
+let feuVert = document.querySelector('#vert');
+let feuOrange = document.querySelector('#orange');
+let feuRouge = document.querySelector('#rouge');
+let buttonAuto = document.querySelector('#btauto');
+let buttonRed = document.querySelector('#btrouge');
+let buttonOrange  = document.querySelector('#btorange');
+let buttonGreen = document.querySelector('#btvert');
+let buttonStop = document.querySelector('#stop');
+let buttonPanne = document.querySelector('#panne');
+let i=0;
+let s;
+let color=[["green","grey","grey"],
+       ["grey","orange","grey"],
+       ["grey","grey","red"]];
+
+//FONCTION FEU AUTOMATIQUE
+function start()
+{   
+        feuRouge.style.backgroundColor=color[i][2];
+        feuOrange.style.backgroundColor=color[i][1];
+        feuVert.style.backgroundColor=color[i][0];
+        i+=1;
+        if(i==3)
+        {
+            i=0;
+        }
+        s=setTimeout(start,1500);
+}
+
+//FONCTION ARRET FEU AUTOMATIQUE
+function stop()
+{
+        buttonAuto.style.visibility="visible";
+        clearTimeout(s);
+        i=0;
+        feuRouge.style.backgroundColor="gray";
+        feuOrange.style.backgroundColor="gray";
+        feuVert.style.backgroundColor="gray";
+}
+
+//FONCTION TEST FEU ROUGE,ORANGE,VERT
+function colorTemp(ColorTest, time)
+{
+    if (ColorTest == 'green')
+    {
+        feuVert.style.backgroundColor=ColorTest;
+            setTimeout(() => 
+        {
+        feuVert.style.backgroundColor="grey";
+        }, time);
+    }
+    else if (ColorTest == 'orange')
+    {
+        feuOrange.style.backgroundColor=ColorTest;
+        setTimeout(() => 
+        {
+            feuOrange.style.backgroundColor="grey";
+        }, time);
+    }
+    else if (ColorTest == 'red')
+    {
+        feuRouge.style.backgroundColor=ColorTest;
+        setTimeout(() => 
+        {
+            feuRouge.style.backgroundColor="grey";
+        }, time);
+    }
+    else
+    {
+        alert('Seuleument les couleurs RED, ORANGE et GREEN sont accepté');
+    }
+}
+
+
+buttonAuto.addEventListener('click', function() {start()});
+buttonStop.addEventListener('click', function(){stop()})
+buttonRed.addEventListener('click', function() {colorTemp('red', 1500)});
+buttonOrange.addEventListener('click', function() {colorTemp('orange', 1500)});
+buttonGreen.addEventListener('click', function() {colorTemp('green', 1500)});
